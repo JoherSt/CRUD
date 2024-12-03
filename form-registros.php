5e393a69314998b25eec18fc32eefcb52647616e
JoherSt <joherstivenfrancoa@gmail.com>
2024-12-03 14:38:33 -0500

    Create form-registros.php
    Create form-registros.php

---
+++ form-registros.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="../MODELO/crud.php" method="post">
+        <label for="">Ingrese sus Nombres</label><br>
+        <input type="text" name="nombres" ><br>
+
+        <label for="">Ingrese su Documento</label><br> 
+        <input type="text" name="documentos" require><br>
+
+        <br>
+
+        <button type="submit">resgistrar</button>
+
+</body>
+</html>
